e904224fdafa7b9f4f33da1dc3a0cfb939a6bed0
MarianDev22 <marianmfalicante@gmail.com>
2026-04-14 21:47:30 +0200

    feat: install + config prettier, eslint y typescript

---
+++ backend/eslint.congig.cjs
@@ -0,0 +1,49 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierConfig = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-use-before-define': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/export': 'error',
+
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+    },
+  },
+  prettierConfig,
+];
---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+
+
+app.use('/example', (req, res) => {
+    const message = "<h1> Hello World </h1>"
+  res.send(message)
+//     res.json({
+//     info: 'first endpoint',
+//   });
+});
+
+app.listen(3000, () => {
+  console.log('up & running on port:', 3000);
+});
