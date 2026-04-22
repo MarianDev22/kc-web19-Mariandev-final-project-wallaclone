c5f8af49bd271bda8f11e90f2bac1f375a58c750
MarianDev22 <marianmfalicante@gmail.com>
2026-04-22 09:19:16 +0200

    feat: add signup user - JWT pending

--- backend/src/controllers/authentication/signupController.ts
+++ backend/src/controllers/authentication/signupController.ts
@@ -1,33 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
 import { authBodyValidator } from './authBodyValidator';
 import { User } from '../../models/User';
+import bcrypt from 'bcrypt';
 
-export const signupController = async (req: Request, res: Response, next:NextFunction) => {
-  const { username, email, password } = authBodyValidator.parse(req.body);
-
-
+export const signupController = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { username, email, password } = authBodyValidator.parse(req.body);
     //rever uso de Promise.all para simplificar código
     const user = await User.findOne({
       email: email,
     });
-    if(user){
-        throw new Error('a user with this email already exists');
+    if (user) {
+      throw new Error('a user with this email already exists');
     }
 
     const user1 = await User.findOne({
       username: username,
     });
-    if(user1){
-        throw new Error(`The username ${username} is taken`);
+    if (user1) {
+      throw new Error(`The username ${username} is taken`);
     }
 
-    //falta hashear contraseña y guardar usuario
-  } catch (error) {
-    next(error)
-  }
+    //hashear contraseña y guardar usuario
+    const hashedPassword = await bcrypt.hash(password, 7);
+    const newUser = new User({ username, email, password: hashedPassword });
 
-  res.status(201).json({
-    content: 'Usuario registrado correctamente',
-  });
+    const createdUser = await newUser.save();
+
+    // esta es la respuesta que tenemos que acordar con Sara de como la vamos a estructurar?
+    res.status(201).json({
+      content: 'Usuario registrado correctamente',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
