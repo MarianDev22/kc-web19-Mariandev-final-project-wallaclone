644e84e283f774d375066e5e2062f92fdb165ef1
MarianDev22 <marianmfalicante@gmail.com>
2026-04-23 14:05:47 +0200

    feat: add sign up

--- backend/src/controllers/authentication/signupController.ts
+++ backend/src/controllers/authentication/signupController.ts
@@ -2,11 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import { authBodyValidator } from './authBodyValidator';
 import { User } from '../../models/User';
 import bcrypt from 'bcrypt';
+import { securityService } from './securityService';
 
 export const signupController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username, email, password } = authBodyValidator.parse(req.body);
     //rever uso de Promise.all para simplificar código
+    const user1 = await User.findOne({
+      username: username,
+    });
+    if (user1) {
+      throw new Error(`The username ${username} is taken`);
+    }
     const user = await User.findOne({
       email: email,
     });
@@ -14,18 +21,13 @@ export const signupController = async (req: Request, res: Response, next: NextFu
       throw new Error('a user with this email already exists');
     }
 
-    const user1 = await User.findOne({
-      username: username,
-    });
-    if (user1) {
-      throw new Error(`The username ${username} is taken`);
-    }
-
     //hashear contraseña y guardar usuario
     const hashedPassword = await bcrypt.hash(password, 7);
     const newUser = new User({ username, email, password: hashedPassword });
 
     const createdUser = await newUser.save();
+
+    const newToken = securityService.generateJWT(newUser);
 
     // esta es la respuesta que tenemos que acordar con Sara de como la vamos a estructurar?
     res.status(201).json({
